91d32f0dc23a88adac6184a4402621a7691a2e46
Felipe Fernandes <felipefernandesmiranda@gmail.com>
2021-12-27 08:30:44 -0300

    integração com o stripe

--- src/pages/api/_lb/manageSubscription.ts
+++ src/pages/api/_lb/manageSubscription.ts
@@ -1,6 +1,22 @@
+import { stripe } from "../../../services/stripe"
+
 export async function saveSubscription(
     subscriptionId: string,
     customerId: string,
 ) {
+    // const userRef = await  
+
+    // const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+    // const subscriptionData = {
+    //     id: subscription.id,
+    //     userId: userRef,
+    //     status: subscription.status,
+    //     price_id: subscription.items.data[0].price.id
+    // }
+
     
+
+
 }
+
